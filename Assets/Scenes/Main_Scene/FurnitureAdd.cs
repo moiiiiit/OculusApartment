7beea13243f5739a48cf0c29766670a898a4594a
Mohit Bhole <mohitb.steam@gmail.com>
2022-04-26 19:37:58 -0500

    Fix furniture freezing

--- Assets/Scenes/Main_Scene/FurnitureAdd.cs
+++ Assets/Scenes/Main_Scene/FurnitureAdd.cs
@@ -4,47 +4,50 @@ using UnityEngine;
 
 public class FurnitureAdd : MonoBehaviour
 {
-    private GameObject couch;
-    private GameObject chair;
-    private GameObject table;
-    private GameObject bed;
-    private GameObject desk;
-    private GameObject office_chair;
+    private GameObject n_couch;
+    private GameObject n_chair;
+    private GameObject n_table;
+    private GameObject n_bed;
+    private GameObject n_desk;
+    private GameObject n_office_chair;
+
+    void Start() {
+        n_couch = GameObject.Find("/Furniture/couch");
+        n_chair = GameObject.Find("/Furniture/DinChair");
+        n_table = GameObject.Find("/Furniture/DinTable");
+        n_bed = GameObject.Find("/Furniture/Bed");
+        n_desk = GameObject.Find("/Furniture/StudyTable");
+        n_office_chair = GameObject.Find("/Furniture/Chair");
+    }
 
     public void OnClickButton()
     {
         
         if(name=="couch"){
-            couch = Instantiate(Resources.Load("couch", typeof(GameObject))) as GameObject;
-            couch.transform.position = new Vector3(-7.15f,0.116262f,24.57f);
-            couch.transform.Rotate(0.0f,0.0f,0.0f);
+            Instantiate(n_couch,new Vector3(-7.15f,0.116262f,24.57f),Quaternion.Euler(0.0f,0.0f,0.0f));
+
         }
         
         else if(name=="chair"){
-            chair= Instantiate(Resources.Load("DinChair", typeof(GameObject))) as GameObject;
-            chair.transform.position = new Vector3(-6.23f,0.1f,31f);
-            chair.transform.Rotate(0.0f,-90.0f,0.0f);
+            Instantiate(n_chair,new Vector3(-6.23f,0.1f,31f),Quaternion.Euler(0.0f,-90.0f,0.0f));
+
         }
         else if(name=="table"){
-            table = Instantiate(Resources.Load("DinTable", typeof(GameObject))) as GameObject;
-            table.transform.localPosition = new Vector3(-6.87f,0.1f,29.14f);
-            table.transform.Rotate(0.0f, 90.0f, 0.0f);
+            Instantiate(n_table,new Vector3(-6.87f,0.1f,29.14f),Quaternion.Euler(0.0f, 90.0f, 0.0f));
+
         }
 
         else if(name=="bed"){
-            bed= Instantiate(Resources.Load("Bed", typeof(GameObject))) as GameObject;
-            bed.transform.position = new Vector3(-6.3245f,0.12f,14.21349f);
-            bed.transform.Rotate(0.0f,180.0f,0.0f);
+            Instantiate(n_bed,new Vector3(-6.3245f,0.12f,14.21349f),Quaternion.Euler(0.0f,180.0f,0.0f));
+
         }
         else if(name=="desk"){
-            desk= Instantiate(Resources.Load("StudyTable", typeof(GameObject))) as GameObject;
-            desk.transform.position = new Vector3(0.504f,0.123f,13.51f);
-            bed.transform.Rotate(0.0f,180.0f,0.0f);
+            Instantiate(n_desk,new Vector3(0.504f,0.123f,13.51f),Quaternion.Euler(0.0f,180.0f,0.0f));
+
         }
         else if(name=="off_chair"){
-            office_chair= Instantiate(Resources.Load("Chair", typeof(GameObject))) as GameObject;
-            office_chair.transform.position = new Vector3(-0.377f,0.12f,13.88249f);
-            office_chair.transform.Rotate(0.0f,-90.0f,0.0f);
+            Instantiate(n_office_chair,new Vector3(-0.377f,0.12f,13.88249f),Quaternion.Euler(0.0f,-90.0f,0.0f));
+
         }
     }
 }
