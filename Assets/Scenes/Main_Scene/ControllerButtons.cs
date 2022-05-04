d8aea4e8492ecfd83d65126b6d13787ba58b7004
Mohit Bhole <mohitb.steam@gmail.com>
2022-05-03 21:55:07 -0500

    Add Usersettings

--- Assets/Scenes/Main_Scene/ControllerButtons.cs
+++ Assets/Scenes/Main_Scene/ControllerButtons.cs
@@ -5,11 +5,6 @@ using System.Linq;
 
 public class ControllerButtons : MonoBehaviour
 {
-    private GameObject searchMenu;
-    private GameObject apartmentsMenu;
-    private GameObject floorPlanMenu;
-    private GameObject character;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -29,55 +24,6 @@ public class ControllerButtons : MonoBehaviour
                 }
             }
         }
-        character = GameObject.Find("/XR");
-        searchMenu = GameObject.Find("/Main UI Group/MainMenuCanvas/Search UI");
-        apartmentsMenu = GameObject.Find("/Main UI Group/MainMenuCanvas/Apartments UI");
-        floorPlanMenu = GameObject.Find("/Main UI Group/FloorPlanCanvas/Floor Plan UI");
-    }
-
-    // Update is called once per frame
-    public void OnClickButton()
-    {
-        if (OVRInput.GetDown(OVRInput.Button.Two))
-        {  //B: Go back
-            
-            if (!searchMenu.active)
-            {
-                if (!apartmentsMenu.active)
-                {
-                    if (!floorPlanMenu.active)
-                    {      //Nothing is open, go to floorplan
-                        searchMenu.SetActive(false);
-                        apartmentsMenu.SetActive(false);
-                        floorPlanMenu.SetActive(true);
-                        character.transform.position = new Vector3(32, 1.5f, 0);
-                        character.transform.rotation = Quaternion.Euler(0, -90, 0);
-                    }
-                    else
-                    {   //Floorplan open, go to apartments
-                        searchMenu.SetActive(false);
-                        apartmentsMenu.SetActive(true);
-                        floorPlanMenu.SetActive(false);
-                    }
-                }
-                else
-                {  //Apartments open, go to main menu (search)
-                    searchMenu.SetActive(true);
-                    apartmentsMenu.SetActive(false);
-                    floorPlanMenu.SetActive(false);
-                }
-            }
-        }
-        else if (OVRInput.GetDown(OVRInput.Button.One))
-        {  //A: Go to home
-            if (!searchMenu.active && !apartmentsMenu.active && !floorPlanMenu.active)
-            {      //Teleport to Main room
-                character.transform.position = new Vector3(32, 1.5f, 0);
-                character.transform.rotation = Quaternion.Euler(0, -90, 0);
-            }
-            searchMenu.SetActive(true);
-            apartmentsMenu.SetActive(false);
-            floorPlanMenu.SetActive(false);
-        }
+        
     }
 }
