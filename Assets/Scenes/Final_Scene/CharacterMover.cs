0ffeba688c83c52fea33597c9bfc9842cbd37316
Ayaan Faraz <ayaanfaraz@gmail.com>
2022-05-05 01:35:15 -0500

    Add working avatar

---
+++ Assets/Scenes/Final_Scene/CharacterMover.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMover : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject XROrigin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CharacterController cc = XROrigin.GetComponent<CharacterController>();
+        cc.attachedRigidbody.mass = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
